f6d4d6f98758a68d0f7eef5b0d0a2656226e7dfa
LETTER Pierre <pierre.letter@safrangroup.com>
2020-08-29 14:01:01 +0200

    Add Other Misc Scripts

---
+++ Other Scripts/AD_Locked_Users.ps1
@@ -0,0 +1,49 @@
+﻿#Setup arrays
+    $LockedUsers = @()
+    $SourceEvents = @()
+ 
+    #Searches for the domain controller with the role of a PDC emulator.
+    $PDCServer = (Get-AdDomain).PDCEmulator
+ 
+    #=======================================================================
+    #Checking locked users
+    Search-ADAccount -LockedOut | Select-Object -first 10 | ForEach-Object{
+        $Object = New-Object PSObject -Property ([ordered]@{ 
+            Name         = $_.name
+            Locked       = $_.lockedout
+            UPN          = $_.UserPrincipalName        
+        })
+        $LockedUsers += $Object
+    }
+    "Currently locked users:"
+    $LockedUsers | Format-Table -AutoSize -Wrap
+    $Users =  $LockedUsers.Name
+ 
+    #=======================================================================
+    #Checking lockout source
+    Try{
+        $SourceEvents = Foreach ($User in $Users){
+            Invoke-Command -cn $PDCServer {param($User)
+                $Object = @{}| Select-Object Username,TimeCreated,Source
+                $Object.Username = $User
+ 
+                #Search for first event ID 4740 in security log - source in the ‘Caller Computer Name’ column.
+                $Events = Get-WinEvent -FilterHashtable @{LogName="Security";ID="4740"} | Where-Object {$_.message -match $User} | Select-Object -first 1
+                If($Events){ 
+                    $Object.Source = $Events.properties.value[1]
+                    $Object.timecreated = $Events.timecreated
+                }
+                Else{ 
+                    $Object.Source = " - "
+                    $Object.timecreated = " - "
+                }
+                $Object
+            } -ArgumentList $User -ErrorAction stop | Select-Object @{n='ServerName';e={$_.pscomputername}},Username,TimeCreated,Source
+        }
+    }
+    Catch [System.Exception]{
+        Write-host "Error" -backgroundcolor red -foregroundcolor yellow
+        $_.Exception.Message
+    }
+    "Lockout source found in $PDCServer :"
+    $SourceEvents | Format-Table -AutoSize -Wrap
---
+++ Other Scripts/Network_Tester_Ports.ps1
@@ -0,0 +1,53 @@
+﻿#Edited by Pierre Letter based on the work of Pawel Janowicz @ https://www.powershellbros.com/
+
+$Servers = Get-Content -Path "FOFTester_Sources.txt"
+$DestinationServ = "server.domain"
+$Array = @()
+$Ports = Get-Content -Path "FOFTester_Ports.txt"
+ 
+Foreach($Server in $Servers)
+{
+    $Obejct = $null
+    $FQDN = $null
+    $Server = $Server.Trim()
+ 
+    Write-Host Processing $Server -ForegroundColor Green
+     
+    #Check FQDN for remote computer 
+    $FQDN = ([System.Net.Dns]::GetHostByName(("$Server")))
+ 
+    If(!$FQDN)
+    {
+        Write-Host "$Server does not exist"
+    }
+    Else
+    {
+        # Create a custom object 
+        $Object = New-Object PSCustomObject
+        $Object | Add-Member -MemberType NoteProperty -Name "Source name" -Value $server
+        $Object | Add-Member -MemberType NoteProperty -Name "Destination name" -Value $DestinationServ
+ 
+        Foreach($Port in $Ports)
+        {
+            $Test = $null
+            Try
+            {
+                $Test = Invoke-Command  -ComputerName $Server -ErrorAction Stop -ScriptBlock{param($Port,$DestinationServ) Test-netconnection -Port $Port -cn $DestinationServ -InformationLevel Quiet } -ArgumentList $Port,$DestinationServ
+                If($Test)
+                {
+                    $Object | Add-Member -MemberType NoteProperty -Name "$Port" -Value $Test
+                }
+            }
+            Catch
+            {
+                Write-Warning $_.Exception.Message
+                $Object | Add-Member -MemberType NoteProperty -Name "$Port" -Value "null"
+            }
+        }
+        $Array += $Object  
+ 
+    }
+}
+Write-Host "Results:" -Foreground Yellow
+$Array | Format-Table -Wrap -AutoSize
+$Array | Export-Csv -Path FOFTest.csv -NoTypeInformation
